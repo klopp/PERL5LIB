e2a285a2e3b66a0d69e1fe3c5196fb053bd5a408
Vsevolod Lutovinov <klopp@yandex.ru>
2023-02-13 11:19:09 +0300

    cget/cset/jget/jset

--- Things/Db.pm
+++ Things/Db.pm
@@ -2,11 +2,15 @@ package Things::Db;
 
 use StdUse;
 
+use Const::Fast;
 use DBI;
+use JSON::XS qw/decode_json encode_json/;
+use Try::Tiny;
 
 use Things::Autoload;
 use base qw/Things::Autoload/;
 
+const my $CONFIG_TABLE => 'config';
 our $VERSION = 'v1.0';
 
 # ------------------------------------------------------------------------------
@@ -26,8 +30,8 @@ sub get_object
 # ------------------------------------------------------------------------------
 sub select_field
 {
-    my ( $self, $select, $field, @attrs ) = @_;
-    my $rc = $self->selectrow_hashref( $select, @attrs );
+    my ( $self, $select, $field, $attrs, @bind ) = @_;
+    my $rc = $self->selectrow_hashref( $select, $attrs || {}, @bind );
     $rc or return;
     return $rc->{$field};
 }
@@ -40,6 +44,54 @@ sub select_fields
     $rc or return;
     my @fields = map { $_->{$field} } @{$rc};
     return wantarray ? @fields : \@fields;
+}
+
+# ------------------------------------------------------------------------------
+sub cget
+{
+    my ( $self, $name ) = @_;
+    return $self->select_field( sprintf( 'SELECT value FROM %s WHERE name = ?', $CONFIG_TABLE ), 'value', undef,
+        $name );
+}
+
+# ------------------------------------------------------------------------------
+sub cset
+{
+    my ( $self, $name, $value ) = @_;
+    my $q = sprintf q{
+            INSERT INTO %s (name, value) VALUES(?, ?)
+                ON CONFLICT(name) DO 
+            UPDATE SET value = ?
+        }, $CONFIG_TABLE;
+    my $sth = $self->prepare($q);
+    return $sth ? $sth->execute( $name, $value, $value ) : $sth;
+}
+
+# ------------------------------------------------------------------------------
+sub jget
+{
+    my ( $self, $name ) = @_;
+    my $rc = $self->cget($name);
+    try {
+        $rc and $rc = decode_json $rc;
+    }
+    catch {
+        undef $rc;
+    };
+    return $rc;
+}
+
+# ------------------------------------------------------------------------------
+sub jset
+{
+    my ( $self, $name, $value ) = @_;
+    try {
+        $value = encode_json $value;
+    }
+    catch {
+        undef $value;
+    };
+    return $value ? $self->cset( $name, $value ) : $value;
 }
 
 # ------------------------------------------------------------------------------
