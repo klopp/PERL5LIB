18c08e1793470dde04b5d263d9802331c1228a0e
Vsevolod Lutovinov <klopp@yandex.ru>
2023-02-17 15:10:27 +0300

    cosmetic

--- Atomic/Task/POE.pm
+++ Atomic/Task/POE.pm
@@ -34,56 +34,26 @@ sub run
         my $end   = $start + $piece;
 
         POE::Session->create(
-            'inline_states' => {
-                '_start' => sub {
-                    print "Session ", $_[SESSION]->ID, " has started.\n";
+            inline_states => {
+                _start => sub {
                     $_[HEAP]->{idx} = $start;
                     $_[HEAP]->{end} = $end;
                     $_[KERNEL]->yield('next_task');
                 },
-                'next_task' => sub {
-
-                    #                    say sprintf 'SID: %s, %u', $_[SESSION]->ID, $_[HEAP]->{idx};
-
-                    #                    for ( $_[HEAP]->{idx} .. $_[HEAP]->{end} ) {
-
+                next_task => sub {
                     my $task = $tasks[ $_[HEAP]->{idx} ];
-
-                    #                        my $task = $tasks[$_];
                     return unless $task;
                     $task->run;
-
-                    #                    }
                     ++$_[HEAP]->{idx};
                     $_[KERNEL]->yield('next_task') if $_[HEAP]->{idx} < $_[HEAP]->{end};
                 },
-
-                #                '_stop' => sub {
-
-                #                    say sprintf 'SID: %s, STOP', $_[SESSION]->ID;
-                #                },
+                _stop => sub {
+                },
             }
         );
     }
 
     POE::Kernel->run;
-
-=pod
-    my @threads;
-    for ( 0 .. $children - 1 ) {
-        my $idx = $piece * $_;
-        push @threads, async {
-
-            for ( $_[0] .. $_[1] ) {
-                my $task = $tasks[$_];
-                last unless $task;
-                $task->run;
-            }
-        }
-        $idx, ( $idx + $piece );
-    }
-    $_->join() for @threads;
-=cut
 
     return;
 }
@@ -92,31 +62,3 @@ sub run
 1;
 __END__
 
-
-=for comment
-    
-use POE;
-
-                for (1..10) {
-                        POE::Session->create(
-                                'inline_states' => {
-                                        '_start'    => sub {
-                                                print "Session ", $_[SESSION]->ID, " has started.\n";
-                                                $_[HEAP]->{'count'} = 0;
-                                                $_[KERNEL]->yield('increment');
-                                        },
-                                        'increment' => sub {
-                                                print "Session ", $_[SESSION]->ID, " counted to", ++$_[HEAP]->{'count'}, ".\n";
-                                                $_[KERNEL]->yield('increment') if $_[HEAP]->{'count'} < 10;
-                                        },
-                                        '_stop'     => sub {
-                                                print "Session ", $_[SESSION]->ID, " has stopped.\n";
-                                        },
-                                }
-                        );
-                }
-
-                POE::Kernel->run;
-    
-    
-=cut
