6d1e7598736014f8181feeda8f6ee66bcc946911
Vsevolod Lutovinov <klopp@yandex.ru>
2023-03-30 20:13:31 +0300

    _decode() only

--- Things/ConfigPP.pm
+++ Things/ConfigPP.pm
@@ -2,11 +2,12 @@ package Things::ConfigPP;
 
 use lib q{.};
 use StdUse;
-use Things qw/:types trim/;
+use Things qw/:types trim xget/;
 
 use Encode qw/decode_utf8/;
 use English qw/-no_match_vars/;
-use List::Util qw/any/;
+
+#use List::Util qw/any/;
 use Try::Tiny;
 
 our $VERSION = 'v1.0';
@@ -14,7 +15,8 @@ our $VERSION = 'v1.0';
 # ------------------------------------------------------------------------------
 sub new
 {
-    my ( $class, $file, @multikeys ) = @_;
+    #    my ( $class, $file, @multikeys ) = @_;
+    my ( $class, $file ) = @_;
 
     my $self;
     if ( !$file ) {
@@ -29,10 +31,10 @@ sub new
             trim $self->{error_};
         }
         else {
-            $self = _lowercase_keys($cfg);
-            my $all = any { $_ eq q{*} or /^all$/ism } @multikeys;
-            $all = undef if any { $_ eq '-' } @multikeys;
-            _multikeys( $self, $all, \@multikeys );
+            #            $self = _lowercase_keys($cfg);
+            #            my $all = any { $_ eq q{*} or /^all$/ism } @multikeys;
+            #            $all = undef if any { $_ eq '-' } @multikeys;
+            #            _multikeys( $self, $all, \@multikeys );
             $self = _decode($self);
         }
     }
@@ -41,25 +43,27 @@ sub new
 
 #------------------------------------------------------------------------------
 sub _decode
-    {
-        my ($src) = @_;
+{
+    my ($src) = @_;
 
-        my $dest;
-        if ( ref $src eq $ARRAY ) {
-            @{$dest} = map { _decode($_) } @{$src};
-        }
-        elsif ( ref $src eq $HASH ) {
-            %{$dest} = map { $_ => _decode( $src->{$_} ) } keys %{$src};
-        }
-        else {
-            try {
-                $dest = decode_utf8 $src;
-            };
-        }
-        return $dest;
+    my $dest;
+    if ( ref $src eq $ARRAY ) {
+        @{$dest} = map { _decode($_) } @{$src};
+    }
+    elsif ( ref $src eq $HASH ) {
+        %{$dest} = map { $_ => _decode( $src->{$_} ) } keys %{$src};
+    }
+    else {
+        try {
+            $dest = decode_utf8 $src;
+        };
+    }
+    return $dest;
 }
 
 #------------------------------------------------------------------------------
+
+=for comment
 sub _multikeys
     {
         my ( $src, $all, $multikeys ) = @_;
@@ -128,21 +132,20 @@ sub _lowercase_keys
         }
         return $dest;
 }
+=cut
 
 # ------------------------------------------------------------------------------
 sub error
-    {
-        my ($self) = @_;
-        return ref $self eq $HASH ? $self->{error_} : undef;
+{
+    my ($self) = @_;
+    return ref $self eq $HASH ? $self->{error_} : undef;
 }
 
 # ------------------------------------------------------------------------------
 sub get
-    {
-        my ( $self, $section, $key ) = @_;
-        return $self->{$section}->{$key} if $key;
-        return $self->{$section} if $section;
-        return $self;
+{
+    my ( $self, $path ) = @_;
+    return xget( $self, $path );
 }
 
 # ------------------------------------------------------------------------------
