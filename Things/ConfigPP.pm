dd262128abfd60aaca35f5f97771817e101e065f
Vsevolod Lutovinov <klopp@yandex.ru>
2023-03-30 21:23:26 +0300

    refactor

--- Things/ConfigPP.pm
+++ Things/ConfigPP.pm
@@ -4,19 +4,20 @@ use lib q{.};
 use StdUse;
 use Things qw/:types trim xget/;
 
+use Const::Fast;
 use Encode qw/decode_utf8/;
 use English qw/-no_match_vars/;
 
-#use List::Util qw/any/;
+use List::Util qw/any/;
 use Try::Tiny;
 
 our $VERSION = 'v1.0';
+const my $PKG_PFX => q{_} . __PACKAGE__ . q{_};
 
 # ------------------------------------------------------------------------------
 sub new
 {
-    #    my ( $class, $file, @multikeys ) = @_;
-    my ( $class, $file ) = @_;
+    my ( $class, $file, @multikeys ) = @_;
 
     my $self;
     if ( !$file ) {
@@ -31,10 +32,10 @@ sub new
             trim $self->{error_};
         }
         else {
-            #            $self = _lowercase_keys($cfg);
-            #            my $all = any { $_ eq q{*} or /^all$/ism } @multikeys;
-            #            $all = undef if any { $_ eq '-' } @multikeys;
-            #            _multikeys( $self, $all, \@multikeys );
+            $self = _lowercase_keys($cfg);
+            my $all = any { $_ eq q{*} or /^all$/ism } @multikeys;
+            $all = undef if any { $_ eq '-' } @multikeys;
+            _multikeys( $self, $all, \@multikeys );
             $self = _decode($self);
         }
     }
@@ -62,77 +63,77 @@ sub _decode
 }
 
 #------------------------------------------------------------------------------
-
-=for comment
 sub _multikeys
-    {
-        my ( $src, $all, $multikeys ) = @_;
+{
+    my ( $src, $all, $multikeys ) = @_;
 
-        if ( ref $src eq $ARRAY ) {
-            _multikeys( $_, $all, $multikeys ) for @{$src};
-        }
-        elsif ( ref $src eq $HASH ) {
-            for my $key ( keys %{$src} ) {
-                if ($all) {
-                    $src->{$key} = [ $src->{$key} ] unless ref $src->{$key} eq $ARRAY;
-                    next;
-                }
-                if ( any { $_ eq $key } @{$multikeys} ) {
-                    $src->{$key} = [ $src->{$key} ] unless ref $src->{$key} eq $ARRAY;
-                }
-                else {
-                    $src->{$key} = pop @{ $src->{$key} } if ref $src->{$key} eq $ARRAY;
+    if ( ref $src eq $ARRAY ) {
+        _multikeys( $_, $all, $multikeys ) for @{$src};
+    }
+    elsif ( ref $src eq $HASH ) {
+        for my $key ( keys %{$src} ) {
+            if ($all) {
+                $src->{$key} = [ $src->{$key} ] unless ref $src->{$key} eq $ARRAY;
+                next;
+            }
+            if ( any { $_ eq $key } @{$multikeys} ) {
+                $src->{$key} = [ $src->{$key} ] unless ref $src->{$key} eq $ARRAY;
+            }
+            else {
+                $src->{$key} = pop @{ $src->{$key} } if ref $src->{$key} eq $ARRAY;
             }
         }
-        }
+    }
 
-        return $src;
+    return $src;
 }
 
 #------------------------------------------------------------------------------
 sub _lowercase_hash
-    {
-        my ($hash) = @_;
-        for ( keys %{$hash} ) {
-            if ( !/^[[:lower:]]+$/sm ) {
-                my $lkey = lc;
-                if ( exists $hash->{$lkey} ) {
-                    if ( ref $hash->{$lkey} ne $ARRAY ) {
-                        $hash->{$lkey} = [ $hash->{$lkey} ];
-                    }
-                    if ( ref $hash->{$_} eq $ARRAY ) {
-                        for my $value ( @{ $hash->{$_} } ) {
-                            push @{ $hash->{$lkey} }, $value;
-                    }
-                    }
-                    else {
-                        push @{ $hash->{$lkey} }, $hash->{$_};
-                }
-                }
-                else {
-                    $hash->{$lkey} = $hash->{$_};
-                }
-                delete $hash->{$_};
+{
+    my ($hash) = @_;
+    for ( keys %{$hash} ) {
+        my $pkey = $PKG_PFX . lc;
+        push @{ $hash->{$pkey} }, $hash->{$_};
+    }
+    return $hash;
+}
+
+#------------------------------------------------------------------------------
+sub _keys_back
+{
+    my ($src) = @_;
+
+    my $dest;
+    if ( ref $src eq $ARRAY ) {
+        @{$dest} = map { _keys_back($_) } @{$src};
+    }
+    elsif ( ref $src eq $HASH ) {
+        my $pkey = $PKG_PFX . '([[:lower:]]+)';
+        for ( keys %{$src} ) {
+            $dest->{$1} = $src->{$_} if /$pkey/;
         }
-        }
-        return $hash;
+    }
+    return $dest;
 }
 
 #------------------------------------------------------------------------------
 sub _lowercase_keys
-    {
-        my ($src) = @_;
+{
+    my ( $src, $level ) = @_;
+    $level ||= 0;
+    my $dest;
+    if ( ref $src eq $ARRAY ) {
+        @{$dest} = map { _lowercase_keys( $_, $level + 1 ) } @{$src};
+    }
+    elsif ( ref $src eq $HASH ) {
+        for ( keys %{$src} ) {
+            push @{ $dest->{ $PKG_PFX . lc } }, $src->{$_};
+        }
+    }
 
-        my $dest;
-        if ( ref $src eq $ARRAY ) {
-            @{$dest} = map { _lowercase_keys($_) } @{$src};
-        }
-        elsif ( ref $src eq $HASH ) {
-            $dest = _lowercase_hash($src);
-        }
-        return $dest;
+    return $level ? $dest : _keys_back($dest);
 }
-=cut
 
 # ------------------------------------------------------------------------------
 sub error
