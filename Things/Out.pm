82d201f0d10b81c539a12167153eff3319a2db62
Vsevolod Lutovinov <klopp@yandex.ru>
2023-04-26 10:44:24 +0300

    cosmetic

--- Things/Out.pm
+++ Things/Out.pm
@@ -3,46 +3,51 @@ package Things::Out;
 # ------------------------------------------------------------------------------
 use strict;
 use warnings;
-
-# ------------------------------------------------------------------------------
 use English qw/-no_match_vars/;
 
 # ------------------------------------------------------------------------------
 use base qw/Exporter/;
-our @EXPORT  = qw/puts fputs perror strerror strerr/;
+our @EXPORT  = qw/puts fputs fprintf perror strerror strerr/;
 our $VERSION = 'v1.0';
 
 # ------------------------------------------------------------------------------
 BEGIN {
     *{strerr} = \&strerror;
-}
+};
 
 # ------------------------------------------------------------------------------
 sub puts
 {
-    my ( $fmt, @args ) = @_;
-    return fputs( *{STDOUT}, $fmt, @args );
+    unshift @_, *{STDOUT};
+    goto &fputs;
 }
 
 # ------------------------------------------------------------------------------
 sub fputs
 {
     my ( $fh, $fmt, @args ) = @_;
-    return print {$fh} sprintf "$fmt\n", @args;
+    return printf {$fh} "$fmt\n", @args;
+}
+
+# ------------------------------------------------------------------------------
+sub fprintf
+{
+    my ( $fh, $fmt, @args ) = @_;
+    return printf {$fh} $fmt, @args;
 }
 
 # ------------------------------------------------------------------------------
 sub strerror
 {
     my ($msg) = @_;
-    defined $msg and return sprintf "%s: %s\n", $msg, $ERRNO;;
+    defined $msg and return sprintf '%s: %s', $msg, $ERRNO;
     return $ERRNO;
 }
 
 # ------------------------------------------------------------------------------
 sub perror
 {
-    return print STDERR strerror(@_);
+    return fputs( *{STDERR}, strerror(@_) );
 }
 
 # ------------------------------------------------------------------------------
