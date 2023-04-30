60165e1243149580abcde1384d239415e681c60b
Vsevolod Lutovinov <klopp@yandex.ru>
2023-05-01 01:49:27 +0300

    KISS

--- Things/Instance.pm
+++ Things/Instance.pm
@@ -10,7 +10,6 @@ use base qw/Exporter/;
 our @EXPORT  = qw/lock_instance/;
 our $VERSION = 'v1.0';
 
-use Const::Fast;
 use English qw/-no_match_vars/;
 use Fcntl qw/:DEFAULT :flock SEEK_SET/;
 use Lock::Socket qw/try_lock_socket/;
@@ -20,36 +19,23 @@ use Net::EmptyPort qw/empty_port/;
 sub lock_instance
 {
     my ($lockfile) = @_;
-    my ( $lock, $port, $fh );
 
+    my ( $lock, $port, $fh );
     if ( !sysopen $fh, $lockfile, O_RDWR | O_CREAT ) {
-        return {
-            errno => $ERRNO,
-            emsg  => sprintf 'Error writing lock file "%s" (%s)!',
-            $lockfile, $ERRNO,
-        };
+        return { errno => $ERRNO, };
     }
-    $fh->autoflush(1);
     sysread $fh, $port, 64;
-    trim($port);
+    $port and $port =~ s/^\s+|\s+$//gsm;
     if ( !$port || $port !~ /^\d+$/sm ) {
         $port = empty_port();
     }
     if ( !( $lock = try_lock_socket($port) ) ) {
         close $fh;
-        return {
-            errno => $ERRNO,
-            emsg  => sprintf 'Application already locked on port %u!',
-            $port,
-        };
+        return { errno => $ERRNO, };
     }
     if ( !flock $fh, LOCK_EX ) {
         close $fh;
-        return {
-            errno => $ERRNO,
-            emsg  => sprintf 'Error locking file "%s" (%s)!',
-            $lockfile, $ERRNO,
-        };
+        return { errno => $ERRNO, };
     }
     sysseek $fh, 0, SEEK_SET;
     syswrite $fh, $port, length $port;
@@ -67,10 +53,7 @@ __END__
  
     use Things::Instance;
     my $rc = lock_instance( "/var/lock/$PROGRAM_NAME.lock" );
-    if( $rc->{errno} ) 
-    {
-        die $rc->{emsg};
-    }
+    $rc->{errno} and Carp::confess $rc->{errno};
     # do something
     undef $rc;
 
