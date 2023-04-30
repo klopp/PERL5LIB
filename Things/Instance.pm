ddf4d5642b957a2cd3a554592ed0c80c465f402d
Vsevolod Lutovinov <klopp@yandex.ru>
2023-04-30 23:26:53 +0300

    1st release

---
+++ Things/Instance.pm
@@ -0,0 +1,81 @@
+package Things::Instance;
+
+# ------------------------------------------------------------------------------
+use strict;
+use warnings;
+use English qw/-no_match_vars/;
+
+# ------------------------------------------------------------------------------
+use base qw/Exporter/;
+our @EXPORT  = qw/lock_instance/;
+our $VERSION = 'v1.0';
+
+use Const::Fast;
+use English qw/-no_match_vars/;
+use Fcntl qw/:DEFAULT :flock SEEK_SET/;
+use Lock::Socket qw/try_lock_socket/;
+use Net::EmptyPort qw/empty_port/;
+
+use Things::Trim;
+
+# ------------------------------------------------------------------------------
+sub lock_instance
+{
+    my ($lockfile) = @_;
+    my ( $lock, $port, $fh );
+
+    if ( !sysopen $fh, $lockfile, O_RDWR | O_CREAT ) {
+        return {
+            errno => $ERRNO,
+            emsg  => sprintf 'Error writing lock file "%s" (%s)!',
+            $lockfile, trim($ERRNO),
+        };
+    }
+    $fh->autoflush(1);
+    sysread $fh, $port, 16;
+    trim($port);
+    if ( !$port || $port !~ /^\d+$/sm ) {
+        $port = empty_port();
+    }
+    if ( !( $lock = try_lock_socket($port) ) ) {
+        close $fh;
+        return {
+            errno => $ERRNO,
+            emsg  => sprintf 'Application locked on port %u!',
+            $port,
+        };
+    }
+    if ( !flock $fh, LOCK_EX ) {
+        close $fh;
+        return {
+            errno => $ERRNO,
+            emsg  => sprintf 'Error locking lock file "%s" (%s)!',
+            $lockfile, trim($ERRNO),
+        };
+    }
+    sysseek $fh, 0, SEEK_SET;
+    syswrite $fh, $port, length $port;
+    close $fh;
+    return $lock;
+}
+
+# ------------------------------------------------------------------------------
+1;
+__END__
+
+=pod
+ 
+=head1 SYNOPSIS
+ 
+    use Things::Instance;
+    my $rc = lock_instance( "/var/lock/$PROGRAM_NAME.lock" );
+    if( $rc->{errno} ) 
+    {
+        die $rc->{emsg};
+    }
+    # do something
+    undef $rc;
+
+=cut
+
+# ------------------------------------------------------------------------------
