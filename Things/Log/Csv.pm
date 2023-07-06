fcb58c452f2f3fed88cebd22d59b14148273b2c2
Vsevolod Lutovinov <klopp@yandex.ru>
2023-07-06 10:59:18 +0300

    created

---
+++ Things/Log/Csv.pm
@@ -0,0 +1,66 @@
+package Things::Log::Csv;
+
+# ------------------------------------------------------------------------------
+use strict;
+use warnings;
+use self;
+
+use Text::CSV;
+
+use Things::Log::File;
+use base qw/Things::Log::File/;
+
+our $VERSION = 'v1.10';
+
+# ------------------------------------------------------------------------------
+#   file => FILE
+#       log file
+#   csv => [ method => value, ... ]
+#       Text::CSV options
+# ------------------------------------------------------------------------------
+sub new
+{
+    $self = $self->SUPER::new(@args);
+    $self->{error} and return $self;
+
+    $self->{csv_}  = Text::CSV->new( $self->{csv} || {} );
+    $self->{error} = Text::CSV->error_diag;
+    return $self;
+}
+
+# ------------------------------------------------------------------------------
+sub plog
+{
+    $self->{csv_}->combine(
+        $self->{log_}->{exe},
+        $self->{log_}->{level},
+        $self->{log_}->{message},
+        $self->{log_}->{pid},
+        $self->{log_}->{tstamp},
+    );
+    my $msg = $self->{csv_}->string;
+    if ($msg) {
+        $self->SUPER::plog($msg);
+    }
+    else {
+        $self->{error} = $self->{csv_}->error_diag;
+    }
+
+    return $self;
+}
+
+# ------------------------------------------------------------------------------
+1;
+__END__
+
+=head1 SYNOPSIS
+
+    my $logger = Things::Log::Csv->new
+    (
+        file => '/var/log/my.log',
+        csv  => [ key => value, ... ]
+    );
+
+=cut
+
+# ------------------------------------------------------------------------------
