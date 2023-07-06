efff29efd821a9994329c78f13a6b489b751317f
Vsevolod Lutovinov <klopp@yandex.ru>
2023-07-06 19:09:33 +0300

    created

---
+++ Things/Log/Code.pm
@@ -0,0 +1,52 @@
+package Things::Log::Code;
+
+# ------------------------------------------------------------------------------
+use strict;
+use warnings;
+use self;
+
+use Things::Const qw/:types/;
+use Things::Log::Base;
+use base qw/Things::Log::Base/;
+
+# ------------------------------------------------------------------------------
+our $VERSION = 'v1.00';
+
+# ------------------------------------------------------------------------------
+sub new
+{
+    $self = $self->SUPER::new(@args);
+    my $coderef = $self->{code} || $self->{coderef};
+    if ( !$coderef ) {
+        $self->{error} = 'No required "code" ("coderef") parameter.';
+        return $self;
+    }
+    if ( ref $coderef ne $CODE ) {
+        $self->{error} = 'Parameter "code" ("coderef") must be CODE ref.';
+        return $self;
+    }
+    $self->{code_} = $coderef;
+    delete $self->{code};
+    return $self;
+}
+
+# ------------------------------------------------------------------------------
+sub plog
+{
+    my ($msg) = @args;
+    $self->{code_}->( $self, $msg );
+    return $self;
+}
+
+# ------------------------------------------------------------------------------
+
+1;
+__END__
+
+=head1 SYNOPSIS
+    
+    my $logger = Things::Log::Code->new( code = sub {} );
+      
+=cut
+
+# ------------------------------------------------------------------------------
