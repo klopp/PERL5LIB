f81a8af847b3162e1d0009d7a8a0d2053b34c3e1
Vsevolod Lutovinov <klopp@yandex.ru>
2023-12-06 18:30:57 +0300

    unused code removed

--- Things/Log.pm
+++ Things/Log.pm
@@ -9,37 +9,12 @@ use English qw/-no_match_vars/;
 use parent qw/Exporter/;
 
 # ------------------------------------------------------------------------------
-use vars qw/$log @EXPORT $VERSION/;
-$VERSION = 'v1.00';
-#@EXPORT  = qw/$log/;
-
-# ------------------------------------------------------------------------------
-=pod
-sub import
-{
-    my ( $self, $module, @params ) = @_;
-
-    $module or return;
-
-    if ( !$module ) {
-        Carp::confess sprintf 'use %s %s::MODULE [, @params];', $self, $self;
-    }
-    $module =~ /^$self/sm or $module = sprintf '%s::%s', $self, $module;
-
-    if ( !$module->can('new') ) {
-        ( my $modfile = $module . '.pm' ) =~ s{::}{/}gsm;
-        eval { require $modfile; 1; } or Carp::confess $EVAL_ERROR;
-    }
-    $log = $module->new(@params);
-    $self->export_to_level( 1, $self, qw/$log/ );
-    return;
-}
-=cut
+our $VERSION = 'v1.10';
 
 # ------------------------------------------------------------------------------
 sub new
 {
-    my ($class, $module, @params ) = @_;
+    my ( $class, $module, @params ) = @_;
     $module =~ /^$class/sm or $module = sprintf '%s::%s', $class, $module;
 
     if ( !$module->can('new') ) {
@@ -53,7 +28,7 @@ sub new
 # ------------------------------------------------------------------------------
 sub DESTROY
 {
-    my ( $self ) = @_;
+    my ($self) = @_;
     return $self->DESTROY;
 }
 
@@ -63,7 +38,7 @@ __END__
 
 =head1 SYNOPSIS
 
-    use Things::Log 'File', file => '/var/log/my.log';
+    my $log = Things::Log->new( 'File', file => '/var/log/my.log' );
     $log->info(...);
 
 =cut
